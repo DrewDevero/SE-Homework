375a0f27db0b05c7807b4c73b9ed202af73d1be8
Drew Devero <deverobelfon@gmail.com>
2020-11-05 13:04:26 -0500

    added link in index.html and made a function into an IIFE

--- Pre-Work-Final/scripts/index.js
+++ Pre-Work-Final/scripts/index.js
@@ -3,12 +3,12 @@
 const MAIN_HEADING = document.getElementById("mainHeading");
 const TEXT_AREA = document.getElementById("textArea");
 
-function constantPhase() {
+(function constantPhase() {
     setTimeout( () => MAIN_HEADING.style.opacity = "0", 500);
     setInterval( () => MAIN_HEADING.style.opacity = "1", 2000);
     setTimeout( () => setInterval( () => MAIN_HEADING.style.opacity = "0", 2000), 1000);
 }
-constantPhase();
+)();
 
 function textAreaSize() {
     if(window.screen.width < 1200 && window.screen.width > 700 || document.body.clientWidth < 1200 && document.body.clientWidth > 700) {
