2f98c1283a54be57d9990c76ce7838ce6cf672ba
Drew Devero <deverobelfon@gmail.com>
2020-11-11 22:23:00 -0500

    added colors to radio button selections, keyframe resizing to submit button, and absolute positioning to submit button

--- Pre-Work-Final/scripts/index.js
+++ Pre-Work-Final/scripts/index.js
@@ -2,6 +2,8 @@
 
 const MAIN_HEADING = document.getElementById("mainHeading");
 const TEXT_AREA = document.getElementById("textArea");
+const INPUTS = document.getElementsByName("power");
+const POWERS = document.getElementsByClassName("powers");
 
 (function constantPhase() {
     setTimeout( () => MAIN_HEADING.style.opacity = "0", 500);
@@ -20,3 +22,18 @@ function textAreaSize() {
     }
 }
 setInterval( () => textAreaSize(), 300);
+
+(function powerColors() {
+    INPUTS[0].addEventListener("mouseover", () => {POWERS[0].style.fontWeight = "bolder"; POWERS[0].style.color = "red";});
+    INPUTS[0].addEventListener("mouseout", () => {POWERS[0].style.fontWeight = "initial"; POWERS[0].style.color = "initial";});
+    INPUTS[1].addEventListener("mouseover", () => {POWERS[1].style.fontWeight = "bolder"; POWERS[1].style.color = "blue";});
+    INPUTS[1].addEventListener("mouseout", () => {POWERS[1].style.fontWeight = "initial"; POWERS[1].style.color = "initial";});
+    INPUTS[2].addEventListener("mouseover", () => {POWERS[2].style.fontWeight = "bolder"; POWERS[2].style.color = "lightgreen;"});
+    INPUTS[2].addEventListener("mouseout", () => {POWERS[2].style.fontWeight = "initial"; POWERS[2].style.color = "initial";});
+    INPUTS[3].addEventListener("mouseover", () => {POWERS[3].style.fontWeight = "bolder"; POWERS[3].style.color = "brown";});
+    INPUTS[3].addEventListener("mouseout", () => {POWERS[3].style.fontWeight = "initial"; POWERS[3].style.color = "initial";});
+    INPUTS[4].addEventListener("mouseover", () => {POWERS[4].style.fontWeight = "bolder"; POWERS[4].style.color = "white";});
+    INPUTS[4].addEventListener("mouseout", () => {POWERS[4].style.fontWeight = "initial"; POWERS[4].style.color = "initial";});
+    INPUTS[5].addEventListener("mouseover", () => POWERS[5].style.fontWeight = "bolder");
+    INPUTS[5].addEventListener("mouseout", () => POWERS[5].style.fontWeight = "initial");
+})();
