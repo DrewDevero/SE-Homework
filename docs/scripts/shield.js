fffa3fa36216b1f7075c8333811ceb020d0f7b46
Drew Devero <deverobelfon@gmail.com>
2020-11-19 22:59:12 -0500

    added text, images, and styling

--- docs/scripts/shield.js
+++ docs/scripts/shield.js
@@ -5,28 +5,16 @@ const POINTS = document.getElementsByClassName("points");
         SHIELD.style.opacity = "1";
 })();
 
-// sizeShift function - makes each word located around the shield polygon re-size and change color, one at a time, in a clockwise sequence
 function pointAnimation(point) { 
     point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
 }
-(function sizeShift() {
-    setTimeout(() => pointAnimation(POINTS[0]), 2000);
-    setTimeout(() => pointAnimation(POINTS[1]), 4000);
-    setTimeout(() => pointAnimation(POINTS[2]), 6000);
-    setTimeout(() => pointAnimation(POINTS[3]), 8000);
-    setTimeout(() => pointAnimation(POINTS[4]), 10000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINTS[0]), 10000)}, 2000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINTS[1]), 10000)}, 4000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINTS[2]), 10000)}, 6000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINTS[3]), 10000)}, 8000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINTS[4]), 10000)}, 10000);
-})();
 
-/* (function pointAnimation(points) {
+// sizeShift function - makes each word located around the shield(id="shield") re-size and change color, one at a time, in a clockwise sequence
+
+(function sizeShift(time) {
 for (let i = 0; i < POINTS.length; i++) {
-    let time = 2000;
-    setTimeout(() => points[i].animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000), time);
-    setTimeout(() => {setInterval(() => points[i].animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000), 10000)}, time);
-    time++;
-}
-})(POINTS); */
+    setTimeout(() => pointAnimation(POINTS[i]), time);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[i]), 10000)}, time);
+    time += 2000;
+    }
+})(2000);
