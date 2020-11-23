24f24e28d955533b55d31a62f448be81d3e48dd9
Drew Devero <deverobelfon@gmail.com>
2020-11-23 14:46:15 -0500

    removed prime number test and added instructors example

--- Labs_and_practice/Mod-1/week-2/Day_2/morningLab.js
+++ Labs_and_practice/Mod-1/week-2/Day_2/morningLab.js
@@ -4,30 +4,48 @@
 4.While loops Fundamentals: https://csx.codesmith.io/units/precourse-part-2/challenge-while-loops-fundamentals
 */
 
-/* let nums = 2;
-let numberCheck = 20;
-let numberCheck_2 = 20;
 
-while (nums < 20) {
-    // only prints numbers that can be made by nums * 1
-    while (numberCheck < 20 && numberCheck > 0) {
-        while (numberCheck_2 < 20 && numberCheck_2 > 0) {
-            if (nums === numberCheck * numberCheck_2) {
-            nums++;
-            } else if (nums !== numberCheck * numberCheck_2 && nums === nums * 1) {
-            console.log(nums);
-            nums++;
-            }
-        numberCheck_2--;
-        }
-    numberCheck --;
+
+// Angel's Code example below:
+// 2, 3, 5, 7, 11, 13, 17, 19
+let i = 0;
+while(i < 20) {
+  if(i === 0) { // this is here because 0 is not prime or composite
+    console.log('the number 0 is neither prime or composite');  
+  } else if(i === 1) {
+    // do nothing lol
+  } else if(i === 2 || i === 3) { // this is here because 2 and 3 are both prime
+    console.log(i);
+  } else {
+    // current iteration : 4 = i
+    //   if number is divisible by any num
+    //    1 - 4
+    //    2 - 3 (inclusive)
+    //    we set isevenlydivisible to true
+    let isevenlydivisible = false;
+    // loop it starts from 2 goes up to i
+    //  and at every step divides i by j (j being the iteration)
+    for(let j = 2; j < i; j++) { // we start from 2 because every num is divisible by 1
+      const hasdecimal = i % j !== 0; // this returns true if the number after dividing by j is whole
+      if(!hasdecimal) { // we're checking if it's not a fraction
+        isevenlydivisible = true; // if it's a whole number then that means at some point it was a whole number when divided
+        // so we say it was at some point evenly divisible
+      }
     }
-    nums++
-} */
+    if(!isevenlydivisible /* if it is prime */) {
+      console.log(i);
+    } // why do we have !isevendivisible?
+        // because if at some point it was evenly divisible it was not a prime
+          // so we do nothing
+    // but when it is a prime
+        // we want to print it
+  }
+  i++;
+}
+// End of Angel's code
 
 /* 2. Use a while loop to add up the numbers 1 to 20 into one variable. Print out the sum at the end
-
-3. Use a while loop to print out the even number from 1 to 20. (You'll need Modulus for this. And an IF Statement.) */
+*/
 
 let sum = 1;
 let num = 1;
@@ -37,6 +55,7 @@ while(num <= 20) {
 }
 console.log(sum);
 
+/* 3. Use a while loop to print out the even number from 1 to 20. (You'll need Modulus for this. And an IF Statement.) */
 let number_2 = 1;
 
 while (number_2 < 21) {
@@ -45,3 +64,10 @@ while (number_2 < 21) {
     }
     number_2++;
 }
+let nums = 2;
+let numberCheck = 2;
+
+while (nums < 20) {
+    // only prints numbers that can be made by nums * 1 and is not divisible by any prime less than or equal to the square root of nums
+    }
+    nums++;
