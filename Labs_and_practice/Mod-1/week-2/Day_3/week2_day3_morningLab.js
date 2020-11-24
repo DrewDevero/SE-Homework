95536b106152f4dedaf2629f64b46d6e8c7789b3
Drew Devero <deverobelfon@gmail.com>
2020-11-24 14:14:44 -0500

    added comments for clarity

--- Labs_and_practice/Mod-1/week-2/Day_3/week2_day3_morningLab.js
+++ Labs_and_practice/Mod-1/week-2/Day_3/week2_day3_morningLab.js
@@ -60,19 +60,28 @@ Write a function vowelCount that takes a String and returns the number of vowels
 */
 
 function vowelCount(string) {
+// establish variable for amount of vowels
     let vowelAmount = 0;
+// list all vowels as a string to eventually check against
     let vowels = "aeiou";
+// loop through all vowels
     for(let i = 0; i < vowels.length; i++) {
+// each time you are on a vowel in vowels loop through your submitted string
         for(let j = 0; j < string.length; j++) {
+// if any letter in the string is equal to the current vowel your on in your loop, increase the amount referenced by vowelAmount by 1
             if(string[j] === vowels[i]) {
                 vowelAmount ++;
             }
         }
     }
+//if the length of your string is greater than 10 log out the statement
     if(string.length > 10) {
         console.log(`${string} is more than 10 characters!`);
     }
+// log your vowelAmount for your string submission
+// return the value (will not show if using node.js)
     console.log(vowelAmount);
     return vowelAmount;
 }
+// call vowelCount with your string submission
 vowelCount("helloTherePeople");
