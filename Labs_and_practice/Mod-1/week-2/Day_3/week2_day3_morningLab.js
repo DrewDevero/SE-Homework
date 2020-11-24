d7a0e23fbbb6a48e68efe19b96760ad20a17ecb1
Drew Devero <deverobelfon@gmail.com>
2020-11-24 12:29:08 -0500

    Week 2 Day 3 Morning Lab

---
+++ Labs_and_practice/Mod-1/week-2/Day_3/week2_day3_morningLab.js
@@ -0,0 +1,69 @@
+/* Write a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
+*/
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+    console.log(num1);
+    return num1;
+    } else {
+    console.log(num2);
+    return num2;
+    }
+}
+
+/* Write a function maxOfThree that takes three numbers as arguments and returns the largest of them. */
+
+function maxOfThreeNumbers(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        console.log(num1);
+        return num1;
+    } else if (num2 > num1 && nun2 > num3) {
+        console.log(num2);
+        return num2;
+    } else {
+        console.log(num3);
+        return num3;
+    }
+}
+
+/* Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise */
+function isCharacterAVowel(character) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    for(let i = 0; i < vowels.length(); i++) {
+        if (character === vowels[i]) {
+        console.log(true);
+        return true;
+        } else {
+        console.log(false);
+        return false;
+        }
+    }
+}
+
+//Write a function charCount that takes a string and returns the length of the string.
+
+function charCount(string) {
+    console.log(string.length());
+    return string.length();
+}
+
+/*
+Write a function vowelCount that takes a String and returns the number of vowels in the String. add a check for the string to be of 10 or less characteers
+*/
+
+function vowelCount(string) {
+    let vowelAmount = 0;
+    let vowels = ["a", "e", "i", "o", "u"];
+    for(let i = 0; i < vowels.length; i++) {
+        for(let j = 0; j < string.length; j++) {
+            if(string[j] === vowels[i]) {
+                vowelAmount ++;
+            }
+        }
+    }
+    if(string.length > 10) {
+        console.log(`${string} is more than 10 characters!`);
+    }
+    console.log(vowelAmount);
+    return vowelAmount;
+}
+vowelCount("helloTherePeople");
