67584689e37877f3a73ea145ac2833c383ab19b0
Drew Devero <deverobelfon@gmail.com>
2020-11-28 04:02:01 -0500

    added more HW bonus work

--- Mod_1/Week_2/HW2_Functions.js
+++ Mod_1/Week_2/HW2_Functions.js
@@ -1,4 +1,4 @@
-/* Write a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
+/*Write a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
 */
 function maxOfTwoNumbers(num1, num2) {
     if (num1 > num2) {
@@ -12,7 +12,7 @@ function maxOfTwoNumbers(num1, num2) {
 
 maxOfTwoNumbers(3, 5);
 
-/* Write a function maxOfThree that takes three numbers as arguments and returns the largest of them. */
+/*Write a function maxOfThree that takes three numbers as arguments and returns the largest of them. */
 
 function maxOfThreeNumbers(num1, num2, num3) {
     if (num1 > num2 && num1 > num3) {
@@ -29,7 +29,7 @@ function maxOfThreeNumbers(num1, num2, num3) {
 
 maxOfThreeNumbers(4, 8, 6);
 
-/* Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise */
+/*Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise */
 function isCharacterAVowel(character) {
     let isAVowel;
     const vowels = "aeiou";
@@ -46,6 +46,7 @@ function isCharacterAVowel(character) {
 }
 isCharacterAVowel("r");
 isCharacterAVowel("e");
+
 //Write a function charCount that takes a string and returns the length of the string.
 
 function charCount(string) {
@@ -55,9 +56,7 @@ function charCount(string) {
 
 charCount("everyone");
 
-/*
-Write a function vowelCount that takes a String and returns the number of vowels in the String. add a check for the string to be of 10 or less characteers
-*/
+/*Write a function vowelCount that takes a String and returns the number of vowels in the String. add a check for the string to be of 10 or less characteers */
 
 function vowelCount(string) {
 // establish variable for amount of vowels
@@ -88,6 +87,9 @@ function vowelCount(string) {
 vowelCount("verycool");
 vowelCount("helloTherePeople");
 
+/*Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+*/
+
 const sumArray = (array) => {
     let sum = 0;
     let product = 1;
@@ -100,19 +102,26 @@ const sumArray = (array) => {
 
 sumArray([1,2,3,4]);
 
+// Write a function that returns the number of arguments passed to the function when called.
+
 function numOfArguments(argument1, argument2, argument3) {
     const argumentArray = [argument1, argument2, argument3];
     console.log(argumentArray.length);
 }
 numOfArguments();
 
-const reverseString = (string) => {
+/*Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj". */
+
+function reverseString(string) {
     const readForwardArray = string.split("");
     const readReverseArray = readForwardArray.reverse((first,last) => last - first);
     const stringReverse = readReverseArray.join("");
     console.log(stringReverse);
+    return stringReverse;
 }
-reverseString("hello");
+reverseString("jag tester");
+
+//Write a function findLongestWord that takes an array of words and returns the length of the longest one.
 
 findLongestWord = (words) => {
     let arrayOfWordLengths = [];
@@ -126,7 +135,9 @@ findLongestWord = (words) => {
 }
 findLongestWord(["hello", "howAreYou", "friend"]);
 
-filterLongestWord = (words, i) => {
+//Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+
+filterLongWords = (words, i) => {
     let arrayOfWordLengths = [];
 // engage each word in the array
     for(let j = 0; j < words.length; j++) {
@@ -137,13 +148,56 @@ filterLongestWord = (words, i) => {
     }
     console.log(arrayOfWordLengths);
 }
-filterLongestWord(["hello", "howAreYou", "friend", "someNiceLearningHere"], 7);
+filterLongWords(["hello", "howAreYou", "friend", "someNiceLearningHere"], 7);
 
-class MakeAReverseStringObj {
-    constructor() {
-        this.reverseString = reverseString;
+//*** I created an object with reverseString method and message method ***
+/* class MakeAReverseStringObj {
+    constructor(word) {
+        this.reverseString = reverseString,
+        this.message = console.log(`I repeat, your reverse string is: ${this.reverseString(word)}.`)
     }
 }
-const string = new MakeAReverseStringObj;
+const string = new MakeAReverseStringObj("Per Scholas");
+string.message;
+*/
 
-string.reverseString("Per Scholas");
+/*Add a method reverseString (from question 6) to the object String so that it is possible to call: "Per Scholas".reverseString(). */
+
+String.prototype.reverseString = function () {
+    reverseString(this);
+}
+"Per Scholas".reverseString();
+
+/*Write a function that takes a string as argument and returns an object where:
+the keys are the characters that occur in the string
+the values are the number of occurrences for each letter, regardless of the case */
+
+function stringKeyAndVal(string) {
+    let strArr = [];
+    for(let i = 0; i < string.length; i++) {
+        if (string[i] !== " ") {
+            strArr.push(string[i]);
+        }
+    }
+    let str = strArr.join("").toLowerCase();
+    console.log(str);
+    class makesAString {
+        constructor() { 
+            for(let i = 0; i < str.length; i++) {
+                this[str[i]] = 0;
+                if (Object.keys(this[str[i]]) === undefined) {
+                    delete Object.keys(this[str[i]]);
+                }
+                console.log(Object.keys(this)[i]);
+                for(let j = 0; j < str.length; j++) {
+                    if(Object.keys(this)[i] === str[j]) {
+                        this[str[i]] += 1;
+                    }
+                }
+            }
+        }
+    }
+    let stringObj = new makesAString;
+    return stringObj;
+}
+console.log(stringKeyAndVal("Per Scholas"));
