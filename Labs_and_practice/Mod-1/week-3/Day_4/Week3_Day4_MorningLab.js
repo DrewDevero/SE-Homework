566e3b396329fa102f0a86d735f6f977daaa34e1
Drew Devero <deverobelfon@gmail.com>
2020-12-01 15:19:50 -0500

    adding HW

--- Labs_and_practice/Mod-1/week-3/Day_4/Week3_Day4_MorningLab.js
+++ Labs_and_practice/Mod-1/week-3/Day_4/Week3_Day4_MorningLab.js
@@ -7,3 +7,31 @@ Define an attribute color
 Create a Nemo object. Can it swim? 
 Find the difference between abstract class and an interface
 Which of the two can have implementation of a method in the class? */
+
+class Fish {
+    constructor(name, color) {
+        this.name = name,
+        this.color = color
+    }
+    swim() {
+        console.log(`${this.name} can swim!`);
+    }
+}
+const CARL = new Fish("Carl", "brown");
+console.log(CARL);
+CARL.swim();
+
+class Nemo extends Fish {
+    constructor(name, color) {
+        super(name, color)
+        this.name = name,
+        this.color = color
+    }
+    canSwim() {
+        this.swim();
+    }
+}
+
+const NEMO = new Nemo("Nemo", "green");
+console.log(NEMO);
+NEMO.canSwim();
