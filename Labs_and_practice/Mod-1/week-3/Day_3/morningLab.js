100db5489422c308d703cd79e67613dee47972e5
Drew Devero <deverobelfon@gmail.com>
2020-12-06 19:42:38 -0500

    reformatted code to class constructor

--- Labs_and_practice/Mod-1/week-3/Day_3/morningLab.js
+++ Labs_and_practice/Mod-1/week-3/Day_3/morningLab.js
@@ -6,23 +6,28 @@ snacks are objects that have a name and a price
 a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
 Be able to call vendingMachine.vend() with a valid integer to return a snack */
 
-let vendingMachine = {
-    snacks: [
-        Snack1 = {
-            name: "Snickers",
-            price: "$2.00"
+class CreateVendingMachine {
+    constructor(name1 = "Snickers", price1 = "$2.00", name2 = "Baby Ruth", price2 = "$1.00", name3 = "Reese's Pieces", price3 = "$3.00") {
+    this.snacks = [
+        {
+            name: name1,
+            price: price1
         }, 
-        Snack2 = {
-            name: "Baby Ruth",
-            price: "$1.00"
+        {
+            name: name2,
+            price: price2
         },
-        Snack3 = {
-            name: "Reese's Pieces",
-            price: "$3.00"
+        {
+            name: name3,
+            price: price3
         }
-    ],
-    vend: (snackPosition) => console.log(vendingMachine.snacks[snackPosition])
+    ]
+    }
+    vend(snackPosition) {
+        console.log(vendingMachine.snacks[snackPosition])
+    }
 }
+vendingMachine = new CreateVendingMachine;
 console.log(vendingMachine);
 vendingMachine.vend(1);
 vendingMachine.vend(2);
