1b10be5c5eee1af294beb1662bc747dc4cd1404b
Drew Devero <deverobelfon@gmail.com>
2020-12-17 15:51:36 -0500

    added REACT_TIC_TAC_TOE lab

---
+++ Labs_and_practice/Mod-2/week1/day1/REACT_TIC_TAC_TOE/app.js
@@ -0,0 +1,73 @@
+const GREETING = "hello tic tac toe";
+const NAME = "Drew";
+/* ReactDOM.render(
+    <div>
+    <h1>{GREETING}</h1>
+    <h2>{`This is ${NAME}`}</h2>
+    </div>,
+    document.getElementById("container")    
+) */
+
+class Header extends React.Component {
+    render() {
+        return (
+         <h1>React Tic Tac Toe</h1>
+        )
+    }
+};
+
+class Player extends React.Component {
+    render() {
+        return (
+         <div class={this.props.whichPlayer}>
+            <h2>Player {this.props.whichPlayer}</h2>
+            <h3>Wins: </h3>
+         </div>
+        )
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="board">
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+            </div>
+        )
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>square</h4>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Player />
+                <Board />
+            </div>
+        )
+    }
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("container")
+);
