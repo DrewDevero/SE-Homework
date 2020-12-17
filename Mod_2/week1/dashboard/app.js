257a6c54ca47933bf15d1d797394b200b884e77a
Drew Devero <deverobelfon@gmail.com>
2020-12-17 16:18:41 -0500

    added HW work - in progress

---
+++ Mod_2/week1/dashboard/app.js
@@ -0,0 +1,7 @@
+/* Recreate Dashboard_Creation_Wireframe_Per_Scholas.png wireframe with React. Be sure to create a few components (e.g. app, sidebar, reviews, average rating, sentiment analysis, website visitors).
+ */
+
+ReactDOM.render(
+    <h1>Commence Dashboard Creation!</h1>,
+    document.querySelector("main")
+);
