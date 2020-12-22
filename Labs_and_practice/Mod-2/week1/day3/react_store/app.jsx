77e3cdbd3e7af585fd0ff390f566c328f45352f5
Drew Devero <deverobelfon@gmail.com>
2020-12-22 11:28:20 -0500

    added lab work

---
+++ Labs_and_practice/Mod-2/week1/day3/react_store/app.jsx
@@ -0,0 +1,84 @@
+/* import Products from "./data"; */
+
+console.log(products);
+
+class App extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+    state = {
+        products: products,
+        name: '',
+        price: 0,
+        description: 'Describe this item'
+      }
+
+      handleChange = (event) =>{
+        this.setState({[event.target.id]: event.target.value})
+      }
+      handleSubmit = (event) =>{
+        event.preventDefault()
+        const newItem = {
+          name: this.state.name,
+          price: this.state.price,
+          description: this.state.description
+        }
+        this.setState({
+          products: [ newItem, ...this.state.products],
+          name: '',
+          price: 0,
+          description: 'Describe this item'
+        })
+      }
+      
+      
+
+      render () {
+        return (
+          <div>
+          <h1> Big Time Shopping </h1>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='name'>Name</label>
+                <input type='text' value={this.state.name} onChange={this.handleChange} id='name' placeholder='name of product' />
+                <br />
+                <label htmlFor='price'>Price</label>
+                <input type='number' value={this.state.price} onChange={this.handleChange} id='price' />
+                <br />
+                <label htmlFor='description'>Description</label>
+                <input type='textarea' value={this.state.description} onChange={this.handleChange} id='description' />
+                <input type="submit" />
+            </form>
+            <div>
+                <h2>Preview our new item</h2>
+                <h3>{this.state.name}</h3>
+                <h4>{this.state.price}</h4>
+                <h5>{this.state.description}</h5>
+            </div>
+            <ul>
+            {this.state.products.map(product => {
+                console.log(product)
+              return (
+                /*<li>{product.name} {product.price}</li> */
+                <ProductList product={product}/> )}
+              )
+            }
+            </ul>
+          </div>
+        )
+      }
+}
+ class ProductList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li>{this.props.product.name}  {this.props.product.price}</li>
+        )
+    }  
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+)
