87b98b2ecf525f349fa9a738b694f613a20667c3
Drew Devero <deverobelfon@gmail.com>
2020-12-24 15:05:56 -0500

    added grocery list file

---
+++ Mod_2/week1/react_groceries/components/groceryList.js
@@ -0,0 +1,23 @@
+const groceryItems = [ 
+    {
+        item: "",
+        brand: "",
+        units: "",
+        quantity: 0,
+        isPurchased: false
+    },
+    {
+        item: "",
+        brand: "",
+        units: "",
+        quantity: 0,
+        isPurchased: false
+    },
+    {
+        item: "",
+        brand: "",
+        units: "",
+        quantity: 0,
+        isPurchased: false
+    }
+];
