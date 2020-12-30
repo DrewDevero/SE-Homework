e2bc60e9eacb728a3a06c4a95bd75e79b517c97b
Drew Devero <deverobelfon@gmail.com>
2020-12-30 18:16:19 -0500

    added comments expressing canvas instructed student-examples work

--- Mod_1/Week_1/day_4/hw3/app.js
+++ Mod_1/Week_1/day_4/hw3/app.js
@@ -1,3 +1,5 @@
+// work coppied from student-examples folder as per canvas assignment instructions
+
 $(()=>{
     const $tiles = $('.tile').on('click', (event)=>{
       $(event.currentTarget).toggleClass('active')  
