7da322f3118781f68c1b0a3a47e9c6bf969a8af5
Drew Devero <deverobelfon@gmail.com>
2021-01-29 00:22:48 -0500

    added final part of HW, resulting in completed HW

--- Mod_3/Mod3_Week1_Day1_Homework/src/org/perscholas/homework/Main.java
+++ Mod_3/Mod3_Week1_Day1_Homework/src/org/perscholas/homework/Main.java
@@ -2,10 +2,15 @@ package org.perscholas.homework;
 
 import static org.perscholas.homework.Reverse.*;
 import static org.perscholas.homework.Prime.*;
+import static org.perscholas.homework.GetUp.*;
 
 public class Main {
     public static void main(String[] args) {
         reverse("Hello HW");
         prime(20);
+        getUp(true, 16);
+        getUp(true, 3);
+        getUp(false, 3);
+        getUp(true, 25);
     }
 }
