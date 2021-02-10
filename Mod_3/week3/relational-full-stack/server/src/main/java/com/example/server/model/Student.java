b381fe9b15efac25515b164d855dbb6c400c0e4e
Drew Devero <deverobelfon@gmail.com>
2021-02-10 11:33:50 -0500

    first commit

---
+++ Mod_3/week3/relational-full-stack/server/src/main/java/com/example/server/model/Student.java
@@ -0,0 +1,71 @@
+package com.example.server.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "school_students")
+public class Student {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private Integer age;
+
+    @Column
+    private Integer grade;
+
+    @Column
+    private String schoolName;
+
+    public Student() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+}
+
